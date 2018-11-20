6bebee049d0b9a8511a01ece9145f6c1ee53a8ba
Hadi Ghattas <hadighattas97@gmail.com>
2018-11-21 01:25:07 +0200

    add graph plotting

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import plyvel
 import bson
 db = plyvel.DB('./tweets', create_if_missing=True)
 
-for key, value in db:
-    print(key.decode("utf-8"))
-    print(bson.loads(value))
+# for key, value in db:
+#     print(key.decode("utf-8"))
+#     print(bson.loads(value))
+
+print(sum(1 for _ in db))
